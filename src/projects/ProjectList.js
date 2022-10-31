596741b30e3ae8a29aa3607b3871152bae81d952
Tony <upstar77@outlook.com>
2022-11-01 08:18:10 +0900

    This is Lab 14 OCmponent communication through Multiple Lvels

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
+import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
+function ProjectList({ projects, onSave }) {
+    const [projectBeeingEdited, setProjectBeeingEdited] = useState({});
 
-function ProjectList({ projects }) {
-    return(
-        <div className="row">
-            {projects.map((project) => (
-                <div key={project.id} className="cols-sm">
-                    <div className="card">
-                        <img src={project.imageUrl} alt={project.name} />
-                        <section className="section dark">
-                            <h5 className="strong">
-                                <strong>{project.name}</strong>
-                            </h5>
-                            <p>{project.description}</p>
-                            <p>Budget: {project.budget.toLocaleString()}</p>
-                        </section>
-                    </div>
-                </div>
-            ))}
+    const handleEdit = (project) => {
+        setProjectBeeingEdited(project);
+    };
+    
+    const cancelEditing = () => {
+        setProjectBeeingEdited({});
+    }
+
+    const items = projects.map(project => (
+        <div key={project.id} className="cols-sm">
+            {project === projectBeeingEdited ? (
+                <ProjectForm 
+                    onSave={onSave}
+                    onCancel={cancelEditing}
+                />
+            ) : (
+                <ProjectCard project={project} onEdit={handleEdit} />
+            )}
         </div>
+    ));
+
+    return(
+        <div className="row">{items}</div>
     );
     
 }
 
 ProjectList.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
+    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
+    onSave: PropTypes.func.isRequired
 };
 
 export default ProjectList;
--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -3,10 +3,18 @@ import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 
 function ProjectsPage() {
+
+    const saveProject = (project) => {
+        console.log('Saving project: ', project);
+    };
+
     return (
         <>
             <h1>Projects</h1>
-            <ProjectList projects={MOCK_PROJECTS} />
+            <ProjectList 
+                onSave={saveProject}
+                projects={MOCK_PROJECTS} 
+            />
         </>
     );
 }
