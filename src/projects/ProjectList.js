d52c3878375f32248c7eda7bb6ea64a1cfe71588
upstar2035 <upstar2035@outlook.com>
2022-12-16 00:02:04 +0900

    This is completed Displaying List Data

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,42 +1,29 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Project } from './Project';
-import ProjectCard from "./ProjectCard";
-import ProjectForm from "./ProjectForm";
-function ProjectList({ projects, onSave }) {
-    const [projectBeeingEdited, setProjectBeeingEdited] = useState({});
+import React from "react";
+import PropTypes from "prop-types";
+import { Project } from "./Project";
 
-    const handleEdit = (project) => {
-        setProjectBeeingEdited(project);
-    };
-    
-    const cancelEditing = () => {
-        setProjectBeeingEdited({});
-    }
-
-    const items = projects.map(project => (
-        <div key={project.id} className="cols-sm">
-            {project === projectBeeingEdited ? (
-                <ProjectForm 
-                    project={project}
-                    onSave={onSave}
-                    onCancel={cancelEditing}
-                />
-            ) : (
-                <ProjectCard project={project} onEdit={handleEdit} />
-            )}
-        </div>
-    ));
-
-    return(
-        <div className="row">{items}</div>
+function ProjectList({projects}) {
+    return (
+        <ul className="row">
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    <div className="card">
+                        <img src={project.imageUrl} alt={project.name} />
+                        <section className="section dark">
+                        <h5 className="strong">
+                            <strong>{project.name}</strong>
+                        </h5>
+                        <p>{project.description}</p>
+                        <p>Budget : {project.budget.toLocaleString()}</p>
+                        </section>
+                    </div>
+                </div>
+            ))}
+        </ul>
     );
-    
 }
 
 ProjectList.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired,
-    onSave: PropTypes.func.isRequired
+    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
 };
-
 export default ProjectList;
